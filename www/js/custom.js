9a57d870de110db58a873f9351cf22d118a6c03a
SrMeuNome <vinicius10001999@gmail.com>
2020-11-28 17:34:49 -0300

    Adicionando conexão com FireBase

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,4 +1,32 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCbD2ZWPs1Vd7itx_S_DlZbHNMM_C5eg2g",
+    authDomain: "prova1-pdm.firebaseapp.com",
+    databaseURL: "https://prova1-pdm.firebaseio.com",
+    projectId: "prova1-pdm",
+    storageBucket: "prova1-pdm.appspot.com",
+    messagingSenderId: "25622153019",
+    appId: "1:25622153019:web:9de2a116eff569e6f0991e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var listaDB = firebase.database().ref('lista')
+
+
 var list = []
+
+const carregarLista = () => {
+    list = []
+    value = {}
+    listaDB.on('value', function (itens) {
+        itens.forEach(function (item, index) {
+            value = { id: item.key, nome: item.val().nome, telefone: item.val().telefone }
+            list.push(value)
+        })
+        refleshList()
+    })
+}
 
 const refleshList = () => {
     var listHTML = []
@@ -15,6 +43,11 @@ const refleshList = () => {
     </li >`)
     })
     $('#list-level').html(listHTML);
+}
+
+const msgAlert = (text) => {
+    $('#error-text').html(text)
+    $('#error').fadeIn('2.5s')
 }
 
 const addList = () => {
@@ -35,18 +68,26 @@ const addList = () => {
         $('#error').fadeIn('2.5s')
     }
     else {
-        list.push({ nome: $('#nome').val(), telefone: $('#telefone').val() })
+        listaDB.push({ nome: $('#nome').val(), telefone: $('#telefone').val() }, (a) => {
+            if (a) {
+                msgAlert("Ocorreu um erro ao adicionar um novo item, verifique sua conexão!")
+            }
+        })
         $('#nome').val('')
         $('#telefone').val('')
         $('label[for="nome"]').attr('class', $('label[for="nome"]').attr('class').replace(/\bactive\b/g, ''))
         $('label[for="telefone"]').attr('class', $('label[for="nome"]').attr('class').replace(/\bactive\b/g, ''))
-        refleshList()
+        carregarLista()
     }
 }
 
 const deleteList = (id) => {
-    list.splice(id, 1)
-    refleshList()
+    listaDB.child(list[id].id).remove((a) => {
+        if (a) {
+            msgAlert("Ocorreu um erro ao deletar um item, verifique sua conexão!")
+        }
+    })
+    carregarLista()
 }
 
 //A regexp /\D/g em conjunto com o replace, está anulando todo caractere não numerico
